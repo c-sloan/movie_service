7948d1991a61c2009bef1fa83184e34a14a3004a
-sloan <cbrum16@gmail.com>
2023-01-11 17:30:27 -0700

    init convert to save user watch list

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import MovieListSchema from "./schema/MovieListSchema.js";
-import movies, { getMovies } from "./routes/movies.js";
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
+import MovieListSchema from "./schema/MovieListSchema.js"
+import movies from "./routes/movies.js"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const MovieList = mongoose.model("movieList", MovieListSchema);
+const app = express()
+const MovieList = mongoose.model("movieList", MovieListSchema)
 
-app.use(express.json());
-app.use('/movies', movies)
+app.use(express.json())
+app.use("/movies", movies)
 
-const port = process.env.PORT || 5500;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const port = process.env.PORT || 5500
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
 
-app.listen(port, () => console.log(`Listening on Port: ${port}`));
+app.listen(port, () => console.log(`Listening on Port: ${port}`))
 
-const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster89537.3rfwxvl.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster89537.3rfwxvl.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   dbName: "horror_movies",
-});
-
-/* const movies = await getMovies(1);
-let totalPages = movies.total_pages; */
-
-/* const createList = async () => {
-  let movieList = movies.results;
-  for (let i = 501; i <= 700; i++) {
-    console.log(i)
-    const newMovies = await getMovies(i);
-    console.log(newMovies)
-    movieList.push(...newMovies?.results);
-  }
-  return movieList;
-};
+})
 
 const saveMovies = async () => {
-  const finalList = await createList();
-  const movieList = new MovieList({ movies: finalList });
+  const finalList = await createList()
+  const movieList = new MovieList({ movies: finalList })
   try {
-    await movieList.save();
+    await movieList.save()
   } catch (err) {
-    console.log(err.message);
+    console.log(err.message)
   }
-}; */
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,60 +1,20 @@
-import express from "express";
-import fetch from "node-fetch";
-import "dotenv/config.js";
+import express from "express"
+import "dotenv/config.js"
 
-let router = express.Router();
-let url = new URL("https://api.themoviedb.org/3/discover/movie");
-const API_KEY = process.env.API_KEY;
-const AUTH_TOKEN = process.env.AUTH_TOKEN;
-
-
-// { id: 27, name: 'Horror' },
-
-const apiOptions = {
-  method: "GET",
-  headers: { Authorization: "Bearer " + AUTH_TOKEN },
-};
-
-router.get("/popular", async (req, res) => {
-  const result = await getPopularMovies()
-  console.log(result)
-  return res.send("Received a GET HTTP method");
-});
+let router = express.Router()
+const API_KEY = process.env.API_KEY
+const AUTH_TOKEN = process.env.AUTH_TOKEN
 
 router.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
+  return res.send("Received a POST HTTP method")
+})
 
 router.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
+  return res.send("Received a PUT HTTP method")
+})
 
 router.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
+  return res.send("Received a DELETE HTTP method")
+})
 
-export const getMovies = async (currPage) => {
-  url.searchParams.append("page", currPage);
-  try {
-    const res = await fetch(url, apiOptions);
-    const json = await res.json();
-    return json;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const getPopularMovies = async (currPage) => {
-  url.searchParams.append("api_key", API_KEY);
-  url.searchParams.append("with_genres", 27);
-  url.searchParams.append("language", "en-US");
-  try {
-    const res = await fetch(url, apiOptions);
-    const json = await res.json();
-    return json.results;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export default router;
+export default router
